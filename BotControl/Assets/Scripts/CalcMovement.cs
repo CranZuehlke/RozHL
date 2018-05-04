22071f6d95afa368fecc59a6075449f30bb490d7
Michael Sattler <michael.sattler@zuehlke.com>
2018-05-04 10:12:47 +0200

    Bot Control project

---
+++ BotControl/Assets/Scripts/CalcMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalcMovement : MonoBehaviour
+{
+
+    public Transform TransCursor;
+    public Transform TransBot;
+
+
+    private bool isMoving;
+    private bool isRotating;
+    private const float destThreshold = 0.1f;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (UpdateBotBos.Instance.IsTracked)
+        {
+            if (!isRotating && AngleDiff() > 1 && AngleDiff() < -1)
+            {
+                InvokeRepeating("RotateBot", 0.5f, 0.25f);
+                isRotating = true;
+            }
+            else if (!isRotating && !isMoving && Vector3.SqrMagnitude(CalcDir()) < destThreshold * destThreshold)
+            {
+                InvokeRepeating("MoveBot", 0.5f, 0.25f);
+                isMoving = true;
+            }
+        }
+    }
+
+    private float AngleDiff()
+    {
+        var botForward = TransBot.forward;
+        var direction = CalcDir();
+        botForward.y = 0;
+        direction.y = 0;
+        return Vector3.SignedAngle(botForward, direction, Vector3.up);
+    }
+
+    private void RotateBot()
+    {
+        if (AngleDiff() > 0)
+        {
+            //Rotation Links
+        }
+        else
+        {
+            //Rotation Rechts
+        }
+
+        if ((AngleDiff() < 1 && AngleDiff() > -1) || !UpdateBotBos.Instance.IsTracked)
+        {
+            CancelInvoke("RotateBot");
+            isRotating = false;
+        }
+    }
+
+    private void MoveBot()
+    {
+        //MoveBot
+
+        if (Vector3.SqrMagnitude(CalcDir()) < destThreshold * destThreshold || !UpdateBotBos.Instance.IsTracked)
+        {
+            CancelInvoke("MoveBot");
+            isMoving = false;
+        }
+    }
+
+    private Vector3 CalcDir()
+    {
+        return TransCursor.position - TransBot.position;
+    }
+
+}
---
+++ BotControl/Assets/Scripts/UpdateBotBos.cs
@@ -0,0 +1,40 @@
+﻿using HoloToolkit.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class UpdateBotBos : Singleton<UpdateBotBos>, ITrackableEventHandler {
+
+    public ImageTargetBehaviour ImageTarget;
+    public bool IsTracked { get; private set; }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if(newStatus==TrackableBehaviour.Status.DETECTED|| newStatus == TrackableBehaviour.Status.TRACKED|| newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            IsTracked = true;
+        }
+        else
+        {
+            IsTracked = false;
+        }
+        throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (IsTracked)
+        {
+            this.transform.position = ImageTarget.gameObject.transform.position;
+            this.transform.rotation = ImageTarget.gameObject.transform.rotation;
+
+        }
+    }
+
+}
